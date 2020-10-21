423ca2188cfac3d29d8a8ceea80787d9e74e912e
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-10-21 18:23:19 -0400

    More efficient

--- rest_live/decorators.py
+++ rest_live/decorators.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 from django.db.models import Model
+from django.db.models.signals import post_delete, post_save
 from rest_framework import serializers
 
 from rest_live import PermissionLambda, __model_to_listeners
+from rest_live.signals import ondelete_callback, onsave_callback
 
 
 def __register_subscription(
@@ -11,6 +13,8 @@ def __register_subscription(
 ):
     model: Model = cls.Meta.model
     label = model._meta.label  # noqa
+    post_save.connect(onsave_callback, model, dispatch_uid="rest-live")
+    post_delete.connect(ondelete_callback, model, dispatch_uid="rest-live")
     if label not in __model_to_listeners:
         __model_to_listeners[label] = dict()
     __model_to_listeners[label][group_key] = (cls, check_permission)
