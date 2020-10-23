2ca881e9b688c55f0f202fdcf666bebe23627dcf
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-10-23 15:51:35 -0400

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 
-VERSION = "0.2.2"
+VERSION = "0.3.0"
 DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 
