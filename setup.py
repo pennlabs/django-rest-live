2a13382e4bb1a60c6e59920f6a7fe6c91e4cdaba
Davis Haupt <davis.haupt@gmail.com>
2021-02-11 17:43:16 -0500

    drop support for python 3.6

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
     classifiers=[
         "Framework :: Django",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
