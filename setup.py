c2fb6cbb43eb2668b999255035a9e8960e966d93
Davis Haupt <davis.haupt@gmail.com>
2021-02-11 17:39:49 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 
