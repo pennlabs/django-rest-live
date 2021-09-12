ff8b20cbd31cd1a3330c3cc081c6b61bed4cb541
Kevin Chen <kevc528@seas.upenn.edu>
2021-09-12 13:38:58 -0400

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 
-VERSION = "0.4.4"
+VERSION = "0.5.0"
 DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 
