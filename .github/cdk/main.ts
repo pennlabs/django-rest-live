3d02f0a1e2e2fb189c5b47e98f05f12aee8d7d51
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-12-04 16:19:19 -0500

    Fix publishing (#17)
    * Update kraken
    
    * Update tox config

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -3,8 +3,6 @@ import { App } from "cdkactions";
 import { PyPIPublishStack } from "@pennlabs/kraken";
 
 const app = new App();
-new PyPIPublishStack(app, {
-  pythonMatrixVersions: [3.7, 3.8, 3.9]
-});
+new PyPIPublishStack(app);
 
 app.synth();
